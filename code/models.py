c00b11b7937f215ac5d7d21e273c58bf9baaf7e6
Yan <astromid@users.noreply.github.com>
2018-01-21 01:23:57 +0300

    BatchNorm problem?

--- code/models.py
+++ code/models.py
@@ -6,7 +6,7 @@ from keras.applications.resnet50 import ResNet50
 
 def resnet50():
     i = Input(shape=(utils.CROP_SIDE, utils.CROP_SIDE, 3))
-    norm_i = BatchNormalization()(i)
+    # norm_i = BatchNormalization()(i)
     base_model = ResNet50(
         include_top=False,
         weights='imagenet',
@@ -14,10 +14,9 @@ def resnet50():
     )
     for layer in base_model.layers:
         layer.trainable = False
-    x = base_model(norm_i)
+    x = base_model(i)
     x = Dense(units=2048, activation='relu')(x)
     x = Dropout(rate=0.2)(x)
     out = Dense(units=utils.N_CLASS, activation='softmax')(x)
     model = Model(inputs=i, outputs=out)
-
     return model
