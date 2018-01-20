90769ace13f850f8cf3c9fa521ec20f31ba903e3
Yan <astromid@users.noreply.github.com>
2018-01-20 21:36:42 +0300

    quick fix, chunksize 10 experiment

--- code/models.py
+++ code/models.py
@@ -5,17 +5,18 @@ from keras.applications.resnet50 import ResNet50
 
 
 def resnet50():
+    i = BatchNormalization(input_shape=(512, 512, 3))
     base_model = ResNet50(
         include_top=False,
         weights='imagenet',
+        input_tensor=i,
         pooling='avg'
     )
-    norm_inp = BatchNormalization()(base_model.input)
     x = base_model.output
     x = Dense(units=2048, activation='relu')(x)
     x = Dropout(rate=0.2)(x)
     out = Dense(units=utils.N_CLASS, activation='softmax')(x)
-    model = Model(inputs=norm_inp, outputs=out)
+    model = Model(inputs=i, outputs=out)
     for layer in base_model.layers:
         layer.trainable = False
     return model
